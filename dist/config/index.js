442b0e8fe5017688e9565af0ca7ed65386c94753
AntoRin_ux <antorinish@gmail.com>
2021-06-25 22:48:18 +0530

    remove dist

--- dist/config/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.config = void 0;
-exports.config = {
-    apiDefault: "http://localhost:8080/api/search",
-    apiYt: "http://localhost:8080/api/search/yt"
-};
-//# sourceMappingURL=index.js.map
